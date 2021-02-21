0b940e91ffb4380d25611ab6664183f09c42c117
Simeon Griggs <simeon@tomoro.com.au>
2021-02-21 19:59:44 +0000

    one-file sitemap

--- web/exportSitemap.js
+++ web/exportSitemap.js
@@ -1,17 +1,42 @@
 const sm = require('sitemap')
 const fs = require('fs')
-const { exportPathMap } = require('./next.config')
+const client = require('./client')
 
-exportPathMap().then((res) => {
-  const sitemap = sm.createSitemap({
-    hostname: `https://recipes.simeongriggs.dev`,
-    cacheTime: 600000, // 600 sec (10 min) cache purge period
+const query = `
+{
+  "routes": *[_type == "recipe"] {
+    _id,
+    slug,
+  }
+}
+`
+
+const reduceRoutes = (obj, route) => {
+  const path = route.slug.current === '/' ? '/' : `/${route.slug.current}`
+  obj[path] = {}
+  return obj
+}
+
+client
+  .fetch(query)
+  .then((res) => {
+    const { routes = [] } = res
+    const nextRoutes = routes
+      .filter(({ slug }) => slug.current)
+      .reduce(reduceRoutes, {})
+
+    return nextRoutes
   })
+  .then((res) => {
+    const sitemap = sm.createSitemap({
+      hostname: `https://recipes.simeongriggs.dev`,
+      cacheTime: 600000, // 600 sec (10 min) cache purge period
+    })
 
-  Object.keys(res).map((page) => sitemap.add({ url: page }))
+    Object.keys(res).map((page) => sitemap.add({ url: page }))
 
-  fs.writeFile(`./public/sitemap.xml`, sitemap.toString(), (err) => {
-    if (err) throw err
-    console.log(`sitemap.xml updated`)
+    fs.writeFile(`./public/sitemap.xml`, sitemap.toString(), (err) => {
+      if (err) throw err
+      console.log(`sitemap.xml updated`)
+    })
   })
-})
--- web/next.config.js
+++ web/next.config.js
@@ -1,44 +1,5 @@
-const client = require('./client')
-
-const query = `
-{
-  "routes": *[_type == "recipe"] {
-    _id,
-    slug,
-    title,
-  }
-}
-`
-
-const reduceRoutes = (obj, route) => {
-  // const { page = {}, slug = {} } = route
-  // const { _createdAt, _updatedAt } = page
-  const path = route.slug.current === '/' ? '/' : `/${route.slug.current}`
-  obj[path] = {
-    // query: {
-    //   slug: slug.current,
-    // },
-    // includeInSitemap,
-    // disallowRobot,
-    // _createdAt,
-    // _updatedAt,
-    // page: '/LandingPage',
-  }
-  return obj
-}
-
 module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
-  },
-  exportPathMap() {
-    return client.fetch(query).then((res) => {
-      const { routes = [] } = res
-      const nextRoutes = routes
-        .filter(({ slug }) => slug.current)
-        .reduce(reduceRoutes, {})
-
-      return nextRoutes
-    })
   },
 }
